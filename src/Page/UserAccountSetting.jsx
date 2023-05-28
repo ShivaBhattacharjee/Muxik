9bfe26cce82c646e0fb433b9edea54060810b896
Shiva <95211406+ShivaBhattacharjee@users.noreply.github.com>
2023-05-28 15:24:52 +0530

    Delete src directory

--- src/Page/UserAccountSetting.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const UserAccountSetting = () => {
-  return <div className="text-neutral-50">This is yet to be developed</div>;
-};
-
-export default UserAccountSetting;
--- src/components/assets/LogoText.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const LogoText = () => {
-  return (
-    <h1 className='font-bold text-3xl'>Muxik</h1>
-  );
-};
-
-export default LogoText;
