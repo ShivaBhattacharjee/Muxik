f9e6dde4ee38c715ea579db5d32eb3299fa4736b
Shiva <shivaxwarrior@gmail.com>
2023-05-29 13:54:28 +0530

    Removed idk some broken thing

--- src/components/SingleSongList.jsx
+++ src/components/SingleSongList.jsx
@@ -5,16 +5,12 @@ import { SongDurtionFormat } from "../Utils/Helper";
 import { SongDownloader } from "../components";
 import Popover from "@mui/material/Popover";
 import Skeleton from "@mui/material/Skeleton";
-import DownloadLogo from "../components/downloader/DownloadLogo";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
-import PlaylistAddIcon from "@mui/icons-material/PlaylistAdd";
 import AlbumIcon from "@mui/icons-material/Album";
 import IconButton from "@mui/material/IconButton";
 import PersonIcon from "@mui/icons-material/Person";
 import { motion, AnimatePresence } from "framer-motion";
 import PopoverPlaylist from "./PopoverPlaylist";
-import DeleteIcon from "@mui/icons-material/Delete";
-import AddIcon from "@mui/icons-material/Add";
 import { Link } from "react-router-dom";
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 const SingleSongList = ({
@@ -31,7 +27,6 @@ const SingleSongList = ({
   playlistId = null,
 }) => {
   const { HandlePlaySong, getSinglePlaylist } = usePlayerContext();
-  const [alert, setAlert] = useState(false);
   const [showPlaylist, setShowPlaylist] = useState(false);
   const [anchorEl, setAnchorEl] = useState(null);
   const [ImageLoading, SetImageLoading] = useState(true);
@@ -39,28 +34,9 @@ const SingleSongList = ({
     SetImageLoading(false);
   }, []);
 
-  let timeoutId;
-  const HandleAlert = useCallback(() => {
-    setAlert(true);
-    if (timeoutId) {
-      clearTimeout(timeoutId);
-    }
-    timeoutId = setTimeout(() => {
-      setAlert(false);
-    }, 5000);
-  }, []);
-
-  const handleClick = useCallback((event) => {
-    setAnchorEl(event.currentTarget);
-  }, []);
 
 
-  const handleClose = useCallback(() => {
-    setShowPlaylist(false);
-    setTimeout(() => {
-      setAnchorEl(null);
-    }, 210);
-  }, []);
+
 
 
   function ArtistFormatter(text) {
@@ -146,105 +122,6 @@ const SingleSongList = ({
 
         <div className="absolute right-4 max-md:right-0 top-3 z-10 flex items-center gap-3 ">
           <SongDownloader songId={id} />
-          <IconButton size="large" onClick={handleClick}>
-            <MoreVertIcon className="text-slate-200 opacity-60" />
-          </IconButton>
-
-          <Popover
-            id={idd}
-            open={open}
-            anchorEl={anchorEl}
-            onClose={handleClose}
-            anchorOrigin={{
-              vertical: "top",
-              horizontal: "center",
-            }}
-            transformOrigin={{
-              vertical: "center",
-              horizontal: "right",
-            }}
-            PaperProps={{
-              sx: {
-                backgroundColor: "#282a2e",
-                paddingY: 1,
-                borderRadius: 3,
-                overflow: "visible",
-                width: "fit-content",
-              },
-            }}
-          >
-
-            {album && album.id && (
-              <Link to={`/album/${album?.id}`}>
-                <ListItemButton
-                  sx={{
-                    paddingRight: 5,
-                    ":hover": {
-                      bgcolor: "#444",
-                    },
-                  }}
-                >
-                  <li className="flex gap-3 text-neutral-200 font-light text-sm ">
-                    <div className="p-1  bg-lightBlue rounded-md">
-                      <AlbumIcon sx={{ fontSize: 20 }} />
-                    </div>
-                    <p>View Album</p>
-                  </li>
-                </ListItemButton>
-              </Link>
-            )}
-
-            {primaryArtistsArr &&
-              primaryArtistsId &&
-              primaryArtistsArr.map((item, index) => {
-                return (
-                  <Link
-                    to={`/artist/${primaryArtistsIdArr[index]?.trim()}`}
-                    key={index}
-                  >
-                    <ListItemButton
-                      sx={{
-                        paddingRight: 5,
-                        ":hover": {
-                          bgcolor: "#444",
-                        },
-                      }}
-                    >
-                      <li className="flex gap-3 text-neutral-200 items-center font-light text-sm ">
-                        <div className="p-1  bg-lightBlue rounded-md">
-                          <PersonIcon sx={{ fontSize: 20 }} />
-                        </div>
-                        <p>{item}</p>
-                      </li>
-                    </ListItemButton>
-                  </Link>
-                );
-              })}
-            <AnimatePresence>
-              {showPlaylist && (
-                <motion.div
-                  initial={{ y: "90%" }}
-                  animate={{ y: "0%" }}
-                  exit={{
-                    opacity: 0,
-                    y: "90%",
-                  }}
-                  className="fixed rounded-t-2xl left-0 right-0 flex bg-opacity-80 backdrop-blur-sm items-center justify-center bottom-0 bg-[#282a2e]   text-neutral-200 text-sm px-2 py-2 rounded-md"
-                >
-                  <div>
-                    <div className="w-full flex justify-center">
-                      <div
-                        className=" bg-neutral-400 rounded-full w-7 mt-1 h-1 mb-3 cursor-pointer"
-                        onClick={handleClose}
-                      ></div>
-                    </div>
-
-                    <PopoverPlaylist songId={id} handleclose={handleClose} />
-                  </div>
-                </motion.div>
-              )}
-            </AnimatePresence>
-          </Popover>
         </div>
       </div>
     </>
