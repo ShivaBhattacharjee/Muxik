3509a5de114e0ad76f3aa8e9775cd086b863c188
Shiva <shivaxwarrior@gmail.com>
2023-07-18 17:57:02 +0530

    fixed typos

--- src/Page/History.jsx
+++ src/Page/History.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const History = () => {
   return (
-    <div className='text-white bg-[#2d1b69]  h-screen'>Hisyor under development</div>
+    <div className='text-white bg-[#2d1b69]  h-screen'>History under development</div>
   )
 }
 
--- src/components/ChartsDisplay.jsx
+++ src/components/ChartsDisplay.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Link } from "react-router-dom";
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/blur.css';
 const ChartsDisplay = ({ title, id, image, name = null }) => {
     return (
         <div className='bg-[#1b103f] p-4 rounded-lg '>
@@ -7,10 +9,10 @@ const ChartsDisplay = ({ title, id, image, name = null }) => {
                     to={`playlists/${id}`}
                     className='flex gap-4 items-center'
                 >
-                    <img
+                    <LazyLoadImage
                         src={image[1].link}
-                        alt=""
-                        loading="lazy"
+                        alt= {title || name}
+                        effect='blur'
                         className="w-24 h-full object-cover rounded-lg"
                     />
                     <h4
