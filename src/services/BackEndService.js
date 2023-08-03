d034d4dec4867bbc9a31228e2a759cfcef596cfe
Shiva <shivaxwarrior@gmail.com>
2023-08-03 16:52:32 +0530

    Added backend

--- src/services/BackEndService.js
+++ src/services/BackEndService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import Cookies from 'js-cookie'; 
 const BackEndService = axios.create({
-  baseURL: 'http://localhost:8080/api', 
+  baseURL: 'https://muxik-backend.vercel.app/api', 
 });
 
 
